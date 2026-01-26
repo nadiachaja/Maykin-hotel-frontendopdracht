42d118cf63a4c7613c78394a5e7f13e992068b21
nadiachaja <nadiagaja13@gmail.com>
2026-01-27 00:21:23 +0100

    #3 skip link

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,31 +9,50 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
+<a href="#main-content" class="skip-link"> Skip to content </a>
 <header>
 	<a href="/">
 		<h1>My Hotels</h1>
 	</a>
 </header>
 
-{@render children()}
+<main id="main-content">
+	{@render children()}
+</main>
 
 <style>
+	.skip-link {
+		position: absolute;
+		z-index: 1000;
+		margin: 1em;
+		padding: 1em;
+		background-color: var(--color-cta);
+		border-radius: .5em;
+		color: var(--secondary-color);
+		text-decoration: none;
+		transform: translateY(-200%);
+		transition: transform 0.3s;
+
+		&:focus {
+			transform: translate(0%);
+		}
+	}
+
 	header {
 		background-color: var(--primary-color);
 		color: var(--secondary-color);
-		padding: .5em;
+		padding: 0.5em;
 	}
 
-	a{
+	a {
 		text-decoration: none;
 	}
 
 	h1 {
-		margin: 0em 0em 0em .5em;
+		margin: 0em 0em 0em 0.5em;
 		padding: 0.5em;
 		font-size: var(--font-size-medium);
 		font-weight: var(--font-weight-normal);
 		color: var(--secondary-color);
-
 	}
 </style>
